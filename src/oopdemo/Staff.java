e447052d15904af65cb6db6235ad6752e6e36444
melancholicbud <pavadim.99@gmail.com>
2022-10-13 12:23:55 +0300

    oopdemo > staff.java
    adding a printMessage() method.

--- src/oopdemo/Staff.java
+++ src/oopdemo/Staff.java
@@ -4,4 +4,8 @@ public class Staff {
     private String nameOfStaff;
     private final int hourlyRate = 30;
     private int hoursWorked;
+
+    public void printMessage() {
+        System.out.println("Calculating Pay...");
+    }
 }
