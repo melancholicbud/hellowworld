de5c6a126817d6cf2e6b9e57e07e9c46604a1d10
melancholicbud <pavadim.99@gmail.com>
2022-10-13 13:11:20 +0300

    test arraymethoddemo
    adding a test-check for array method demo. that's it for today.

--- src/arraymethoddemo/ArrayMethodDemo.java
+++ src/arraymethoddemo/ArrayMethodDemo.java
@@ -1,5 +1,7 @@
 package arraymethoddemo;
 
+import java.util.Arrays;
+
 class MyClass {
     public void printFirstElement(int[] a) {
         System.out.println("The first element is " + a[0]);
@@ -15,5 +17,13 @@ class MyClass {
 }
 
 public class ArrayMethodDemo {
-    
+    public static void main(String[] args) {
+        MyClass amd = new MyClass();
+
+        int[] myArray = {1, 2, 3, 4, 5};
+        amd.printFirstElement(myArray);
+
+        int[] myArray2 = amd.returnArray();
+        System.out.println(Arrays.toString(myArray2));
+    }
 }
