e1210f61f478c1b2097cac8d461abd0d5659ebb6
melancholicbud <pavadim.99@gmail.com>
2022-10-13 12:21:22 +0300

    oopdemo
    getting knowledge about OOP in Java

---
+++ src/oopdemo/ObjectOrientedDemo.java
@@ -0,0 +1,3 @@
+package oopdemo;
+public class ObjectOrientedDemo {
+}
---
+++ src/oopdemo/Staff.java
@@ -0,0 +1,7 @@
+package oopdemo;
+
+public class Staff {
+    private String nameOfStaff;
+    private final int hourlyRate = 30;
+    private int hoursWorked;
+}
