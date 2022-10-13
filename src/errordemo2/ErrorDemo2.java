fdf8f23a615163bc4e7c48868f6db29629282636
melancholicbud <pavadim.99@gmail.com>
2022-10-13 11:35:05 +0300

    errordemo2
    another example of try-catch statement but with own errors

---
+++ src/errordemo2/ErrorDemo2.java
@@ -0,0 +1,29 @@
+package errordemo2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ErrorDemo2 {
+    public static void main(String[] args) {
+
+        int choice = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = { 10, 11, 12, 13, 14, 15};
+        System.out.print("Please, etner the index of the array: ");
+        try {
+            choice = input.nextInt();
+            System.out.printf("numbers[%d] = %d%n", choice, numbers[choice]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Index is invalid.");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Error: You did not enter an integer.");
+        }
+        catch (Exception e) {
+            System.out.printf(e.getMessage());
+        }
+    }
+}
