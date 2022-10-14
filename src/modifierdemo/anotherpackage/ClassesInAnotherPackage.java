d33927af51af43d5437184e9a3d1c67370ac4498
melancholicbud <pavadim.99@gmail.com>
2022-10-14 10:13:30 +0300

    making a new package
    new package has been added & showing off more errors that is normal here, don't worry.

---
+++ src/modifierdemo/anotherpackage/ClassesInAnotherPackage.java
@@ -0,0 +1,39 @@
+package modifierdemo.anotherpackage;
+import modifierdemo.ModifierDemo;
+
+public class ClassesInAnotherPackage {
+    // another empty class
+}
+
+class ClassC extends ModifierDemo {
+    public void printMessages() {
+        // allowed
+        System.out.println(publicNum);
+
+        // allowed
+        System.out.println(protectedNum);
+
+        // NOT allowed
+        System.out.println(packagePrivateNum);
+
+        // NOT allowed
+        System.out.println(privateNum);
+    }
+}
+
+class ClassD {
+    public void printMessages() {
+        ModifierDemo p = new ModifierDemo();
+        // allowed
+        System.out.println(p.publicNum);
+
+        // NOT allowed
+        System.out.println(p.protectedNum);
+
+        // NOT allowed
+        System.out.println(p.packagePrivateNum);
+
+        // NOT allowed
+        System.out.println(p.privateNum);
+    }
+}
