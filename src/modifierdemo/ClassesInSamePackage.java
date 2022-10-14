ee859c029e372a782af9dffad63ddbfca00262f7
melancholicbud <pavadim.99@gmail.com>
2022-10-14 11:12:52 +0300

    commenting code with errors
    due for building issues via IDE I commented these codes.

--- src/modifierdemo/ClassesInSamePackage.java
+++ src/modifierdemo/ClassesInSamePackage.java
@@ -1,38 +1,38 @@
-package modifierdemo;
-
-public class ClassesInSamePackage {
-    // empty class
-}
-
-class ClassA extends ModifierDemo {
-    public void printMessages() {
-        // allowed
-        System.out.println(publicNum);
-
-        // allowed
-        System.out.println(protectedNum);
-
-        // allowed
-        System.out.println(packagePrivateNum);
-
-        // NOT allowed
-        System.out.println(privateNum);
-    }
-}
-
-class ClassB {
-    public void printMessages() {
-        ModifierDemo p = new ModifierDemo();
-        // allowed
-        System.out.println(p.publicNum);
-
-        // allowed
-        System.out.println(p.protectedNum);
-
-        // allowed
-        System.out.println(p.packagePrivateNum);
-
-        // NOT allowed
-        System.out.println(p.privateNum);
-    }
-}
+//package modifierdemo;
+//
+//public class ClassesInSamePackage {
+//    // empty class
+//}
+//
+//class ClassA extends ModifierDemo {
+//    public void printMessages() {
+//        // allowed
+//        System.out.println(publicNum);
+//
+//        // allowed
+//        System.out.println(protectedNum);
+//
+//        // allowed
+//        System.out.println(packagePrivateNum);
+//
+//        // NOT allowed
+//        System.out.println(privateNum);
+//    }
+//}
+//
+//class ClassB {
+//    public void printMessages() {
+//        ModifierDemo p = new ModifierDemo();
+//        // allowed
+//        System.out.println(p.publicNum);
+//
+//        // allowed
+//        System.out.println(p.protectedNum);
+//
+//        // allowed
+//        System.out.println(p.packagePrivateNum);
+//
+//        // NOT allowed
+//        System.out.println(p.privateNum);
+//    }
+//}
