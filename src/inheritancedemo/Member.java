8eb29f4047a9d4a754e1f4411e2b24b117ddb8f3
melancholicbud <pavadim.99@gmail.com>
2022-10-14 09:07:45 +0300

    6 fields for inheritancedemo
    welcome message, annual fee, name, member ID & discount

--- src/inheritancedemo/Member.java
+++ src/inheritancedemo/Member.java
@@ -1,4 +1,11 @@
 package inheritancedemo;
+import java.util.Scanner;
 
 public class Member {
+    public String welcome = "Welcome to GYM Fitness";
+    protected double annualFee;
+    private String name;
+    private int memberID;
+    private int memberSince;
+    private int discount;
 }
