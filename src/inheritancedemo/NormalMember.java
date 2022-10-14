55c4cca9b5f58341b0be62cd9827333304d304f4
melancholicbud <pavadim.99@gmail.com>
2022-10-14 09:24:56 +0300

    NormalMember constructors
    adding 2 constructor without & with parameters in NormalMember class.

--- src/inheritancedemo/NormalMember.java
+++ src/inheritancedemo/NormalMember.java
@@ -1,4 +1,13 @@
 package inheritancedemo;
 
 public class NormalMember extends Member {
+
+    public NormalMember() {
+        System.out.println("Child constructor with no parameter");
+    }
+
+    public NormalMember(String pName, int pMemberID, int pMemberSince) {
+        super(pName, pMemberID, pMemberSince);
+        System.out.println("Child Constructor with 3 parameters");
+    }
 }
