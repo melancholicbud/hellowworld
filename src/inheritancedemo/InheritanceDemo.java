e36f947d3b4daf121d8b5ecc9e456b6a16ee8636
melancholicbud <pavadim.99@gmail.com>
2022-10-14 09:34:10 +0300

    InheritanceDemo showcase
    updating InheritanceDemo.java code with testing methods.

--- src/inheritancedemo/InheritanceDemo.java
+++ src/inheritancedemo/InheritanceDemo.java
@@ -1,4 +1,14 @@
 package inheritancedemo;
 
 public class InheritanceDemo {
+    public static void main(String[] args) {
+        NormalMember mem1 = new NormalMember("James", 1, 2010);
+        VIPMember mem2 = new VIPMember("Andy", 2, 2011);
+
+        mem1.calculateAnnualFee();
+        mem2.calculateAnnualFee();
+
+        mem1.displayMemberInfo();
+        mem2.displayMemberInfo();
+    }
 }
