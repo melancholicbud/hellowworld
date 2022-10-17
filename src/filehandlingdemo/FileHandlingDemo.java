481ef0b4d58a913f5a1ad62f9152ad5185f45a48
melancholicbud <pavadim.99@gmail.com>
2022-10-17 10:40:15 +0300

    rewrite code
    but - surprise - it doesn't work either

--- src/filehandlingdemo/FileHandlingDemo.java
+++ src/filehandlingdemo/FileHandlingDemo.java
@@ -5,25 +5,37 @@ public class FileHandlingDemo {
 
     public static void main(String[] args) {
 
-        String line;
-        BufferedReader reader = null;
+//        String line;
+//        BufferedReader reader = null;
+//
+//        try {
+//            reader = new BufferedReader(new FileReader("myFile.txt"));
+//            line = reader.readLine();
+//            while (line != null) {
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        } finally {
+//            try {
+//                if (reader != null)
+//                    reader.close();
+//            } catch (IOException e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
 
-        try {
-            reader = new BufferedReader(new FileReader("myFile.txt"));
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.txt"))) {
             line = reader.readLine();
             while (line != null) {
                 System.out.println(line);
                 line = reader.readLine();
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (reader != null)
-                    reader.close();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
         }
     }
 }
