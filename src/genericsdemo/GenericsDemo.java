d6680fa2db86bcc1aa40ab36b600af55e79bf4b3
melancholicbud <pavadim.99@gmail.com>
2022-10-17 11:08:39 +0300

    Update GenericsDemo.java
    integer in T-pose

--- src/genericsdemo/GenericsDemo.java
+++ src/genericsdemo/GenericsDemo.java
@@ -4,15 +4,15 @@ public class GenericsDemo {
     public static void main(String[] args) {
         MyGenericsClass g = new MyGenericsClass();
 
-        g.setMyVar(6);
+        g.setMyVar("Java");
         g.printValue();
     }
 }
 
-class MyGenericsClass {
-    private Integer myVar;
+class MyGenericsClass<T> {
+    private T myVar;
 
-    void setMyVar(Integer i) {
+    void setMyVar(T i) {
         myVar = i;
     }
 
