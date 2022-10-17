f90b6e983122cc7092af17b4b4d803ef9f3f495f
melancholicbud <pavadim.99@gmail.com>
2022-10-17 10:57:34 +0300

    writing file
    now we write something in file, but due my reasons it doesn't work for me

--- src/filehandlingdemo/FileHandlingDemo.java
+++ src/filehandlingdemo/FileHandlingDemo.java
@@ -26,13 +26,21 @@ public class FileHandlingDemo {
 //            }
 //        }
 
-        String line;
-        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.txt"))) {
-            line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = reader.readLine();
-            }
+//        String line;
+//        try (BufferedReader reader = new BufferedReader(new FileReader("myFile.txt"))) {
+//            line = reader.readLine();
+//            while (line != null) {
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
+//        }
+//        catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+        String text = "Hellow world";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("myFile2.txt", true))) {
+            writer.write(text);
+            writer.newLine();
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
