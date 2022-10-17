3a1cb581fb63b361c411b794ba041f297ff37907
melancholicbud <pavadim.99@gmail.com>
2022-10-17 11:06:02 +0300

    MyGenericsClass expansion
    setMyVar & printValue methods are described.

--- src/genericsdemo/GenericsDemo.java
+++ src/genericsdemo/GenericsDemo.java
@@ -11,5 +11,12 @@ public class GenericsDemo {
 
 class MyGenericsClass {
     private Integer myVar;
-    
+
+    void setMyVar(Integer i) {
+        myVar = i;
+    }
+
+    void printValue() {
+        System.out.println("The value of myVar is " + myVar);
+    }
 }
